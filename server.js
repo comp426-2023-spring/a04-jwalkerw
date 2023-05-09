ae10ec92eaba24dfd465e68ad890dc2f2268a034
Jordan Wahab <jordan_wahab@unc.edu>
2023-05-09 00:29:02 -0400

    Update server.js

--- server.js
+++ server.js
@@ -14,15 +14,15 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 // Endpoints and Plays
-app.get('/app/', (req, res) => { res.status(200).send("200 OK"); });
-app.get('/app/rps/', (req, res) => { res.status(200).send(rps(null)); });
-app.get('/app/rpsls/', (req, res) => { res.status(200).send(rpsls(null)); });
-app.get('/app/rps/play/', (req, res) => { res.status(200).send(rps(req.query.shot)); });
-app.get('/app/rps/play/', (req, res) => { res.status(200).send(rps(req.body.shot)); });
-app.get('/app/rps/play/:shot/', (req, res) => { res.status(200).send(rps(req.params.shot)); });
-app.get('/app/rpsls/play/', (req, res) => { res.status(200).send(rpsls(req.query.shot)); });
-app.get('/app/rpsls/play/', (req, res) => { res.status(200).send(rpsls(req.body.shot)); });
-app.get('/app/rpsls/play/:shot/', (req, res) => { res.status(200).send(rpsls(req.params.shot)); });
+app.get('/app', (req, res) => { res.status(200).send("200 OK"); });
+app.get('/app/rps', (req, res) => { res.status(200).send(rps(null)); });
+app.get('/app/rpsls', (req, res) => { res.status(200).send(rpsls(null)); });
+app.get('/app/rps/play', (req, res) => { res.status(200).send(rps(req.query.shot)); });
+app.get('/app/rps/play', (req, res) => { res.status(200).send(rps(req.body.shot)); });
+app.get('/app/rps/play/:shot', (req, res) => { res.status(200).send(rps(req.params.shot)); });
+app.get('/app/rpsls/play', (req, res) => { res.status(200).send(rpsls(req.query.shot)); });
+app.get('/app/rpsls/play', (req, res) => { res.status(200).send(rpsls(req.body.shot)); });
+app.get('/app/rpsls/play/:shot', (req, res) => { res.status(200).send(rpsls(req.params.shot)); });
 app.get('*', (req, res) => { res.status(400).send("404 NOT FOUND"); });
 
 //Listen
